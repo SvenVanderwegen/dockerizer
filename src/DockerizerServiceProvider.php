c8185554b2a7820a77d268e51799ac09be86cd61
Sven Vanderwegen <sven@vbc.be>
2025-04-18 21:47:47 +0200

    chore: update project details and add Dockerizer service provider

---
+++ src/DockerizerServiceProvider.php
@@ -0,0 +1,16 @@
+<?php
+
+declare(strict_types=1);
+
+namespace SvenVanderwegen\Dockerizer;
+
+/**
+ * @internal
+ */
+final class DockerizerServiceProvider
+{
+    public function register(): void
+    {
+        // Register the service provider.
+    }
+}
